3c66c7238298b67e5e2be477408b7e7aa7c9136d
santosh <santosh092sharma@gmail.com>
2015-09-30 20:47:11 +0530

    Add a simiple output change.

--- SalesTool/src/com/santosh/Main.java
+++ SalesTool/src/com/santosh/Main.java
@@ -5,12 +5,15 @@ public class Main
 public static void main(String args[])
 {
 	SalesData data = new SalesData();
+	data.show();
 	display();
 }
 public static void display()
 {
 	System.out.println("Hello Sales People");
 	System.out.println("This app show sales data");
+	
+	System.out.println("Test 1");
 	
 }
 }

fdc293da9f9646054eb1b9221522c9f4006342c2
santosh <santosh092sharma@gmail.com>
2015-09-30 19:59:22 +0530

    Initial checking sales tool

---
+++ SalesTool/src/com/santosh/Main.java
@@ -0,0 +1,16 @@
+package com.santosh;
+
+public class Main 
+{
+public static void main(String args[])
+{
+	SalesData data = new SalesData();
+	display();
+}
+public static void display()
+{
+	System.out.println("Hello Sales People");
+	System.out.println("This app show sales data");
+	
+}
+}
---
+++ SalesTool/src/com/santosh/SalesData.java
@@ -0,0 +1,9 @@
+package com.santosh;
+
+public class SalesData 
+{
+public void show()
+{
+System.out.println("TOOO Implement data display");	
+}
+}
